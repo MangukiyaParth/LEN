87a8c6d08e7be8e9efca3e66eb887645643519b6
Parth <mparth141@gmail.com>
2023-12-20 17:30:05 +0530

    update DB

--- db.sql
+++ db.sql
@@ -68,11 +68,28 @@ CREATE TABLE `tbl_audit_logs` (
   `is_deleted` tinyint(1) DEFAULT NULL,
   `ip_address` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=17030567 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=17030732 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 /*Data for the table `tbl_audit_logs` */
 
-insert  into `tbl_audit_logs`(`id`,`record_id`,`user_id`,`action`,`operation`,`from`,`status`,`date_added`,`date_modified`,`custom_text`,`is_deleted`,`ip_address`) values (17030493,0,1,'Add','add_songs','panel',0,'2023-12-20 05:15:18','2023-12-20 05:15:18','',1,'::1'),(17030494,0,1,'Add','add_songs','panel',1,'2023-12-20 05:17:58','2023-12-20 05:17:58','',1,'::1'),(17030496,0,1,'Add','add_songs','panel',1,'2023-12-20 05:21:08','2023-12-20 05:21:08','',1,'::1'),(17030497,0,1,'Add','add_songs','panel',1,'2023-12-20 05:22:19','2023-12-20 05:22:19','',1,'::1'),(17030498,0,1,'Add','add_songs','panel',1,'2023-12-20 05:24:40','2023-12-20 05:24:40','',1,'::1'),(17030566,0,1,'Add','add_song_like','panel',1,'2023-12-20 12:46:47','2023-12-20 12:46:47','',1,'::1');
+insert  into `tbl_audit_logs`(`id`,`record_id`,`user_id`,`action`,`operation`,`from`,`status`,`date_added`,`date_modified`,`custom_text`,`is_deleted`,`ip_address`) values (17030493,0,1,'Add','add_songs','panel',0,'2023-12-20 05:15:18','2023-12-20 05:15:18','',1,'::1'),(17030494,0,1,'Add','add_songs','panel',1,'2023-12-20 05:17:58','2023-12-20 05:17:58','',1,'::1'),(17030496,0,1,'Add','add_songs','panel',1,'2023-12-20 05:21:08','2023-12-20 05:21:08','',1,'::1'),(17030497,0,1,'Add','add_songs','panel',1,'2023-12-20 05:22:19','2023-12-20 05:22:19','',1,'::1'),(17030498,0,1,'Add','add_songs','panel',1,'2023-12-20 05:24:40','2023-12-20 05:24:40','',1,'::1'),(17030566,0,1,'Add','add_song_like','panel',1,'2023-12-20 12:46:47','2023-12-20 12:46:47','',1,'::1'),(17030577,0,1,'Add','add_comment','panel',1,'2023-12-20 13:06:32','2023-12-20 13:06:32','',1,'::1'),(17030592,0,1,'Add','add_to_playlist','panel',1,'2023-12-20 13:30:59','2023-12-20 13:30:59','',1,'::1'),(17030593,0,1,'Add','add_to_playlist','panel',1,'2023-12-20 13:31:48','2023-12-20 13:31:48','',1,'::1'),(17030594,0,1,'Add','add_to_playlist','panel',1,'2023-12-20 13:33:24','2023-12-20 13:33:24','',1,'::1'),(17030596,0,1,'Update','update_playlist_name','panel',1,'2023-12-20 13:38:09','2023-12-20 13:38:09','',1,'::1'),(17030597,0,1,'Update','update_playlist_name','panel',1,'2023-12-20 13:38:25','2023-12-20 13:38:25','',1,'::1'),(17030709,0,1,'Update','update_profile','panel',1,'2023-12-20 16:45:40','2023-12-20 16:45:40','',1,'::1'),(17030721,0,0,'Add','add_review','panel',1,'2023-12-20 17:05:46','2023-12-20 17:05:46','',1,'::1'),(17030725,0,2,'Add','add_review','panel',1,'2023-12-20 17:11:51','2023-12-20 17:11:51','',1,'::1'),(17030731,0,2,'Add','add_review','panel',1,'2023-12-20 17:22:05','2023-12-20 17:22:05','',1,'::1');
+
+/*Table structure for table `tbl_comment_song` */
+
+DROP TABLE IF EXISTS `tbl_comment_song`;
+
+CREATE TABLE `tbl_comment_song` (
+  `id` varchar(50) NOT NULL,
+  `song_id` varchar(50) DEFAULT NULL,
+  `comment` longtext DEFAULT NULL,
+  `entry_by` varchar(50) DEFAULT NULL,
+  `entry_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Data for the table `tbl_comment_song` */
+
+insert  into `tbl_comment_song`(`id`,`song_id`,`comment`,`entry_by`,`entry_at`) values ('17030577-9202-1037-d5e8-c0b7e76d4449','17030498-8055-1368-37e8-c93422df4151','Test','1','2023-12-20 13:06:32');
 
 /*Table structure for table `tbl_genre` */
 
@@ -105,6 +122,66 @@ CREATE TABLE `tbl_like_song` (
 
 /*Data for the table `tbl_like_song` */
 
+/*Table structure for table `tbl_playlist` */
+
+DROP TABLE IF EXISTS `tbl_playlist`;
+
+CREATE TABLE `tbl_playlist` (
+  `id` varchar(50) NOT NULL,
+  `playlist` varchar(150) DEFAULT NULL,
+  `entry_by` varchar(50) DEFAULT NULL,
+  `entry_at` datetime DEFAULT NULL,
+  `update_by` varchar(50) DEFAULT NULL,
+  `update_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Data for the table `tbl_playlist` */
+
+insert  into `tbl_playlist`(`id`,`playlist`,`entry_by`,`entry_at`,`update_by`,`update_at`) values ('17030593-8786-2473-d46f-019662e546c2','New List','1','2023-12-20 13:33:07','1','2023-12-20 13:38:25');
+
+/*Table structure for table `tbl_playlist_details` */
+
+DROP TABLE IF EXISTS `tbl_playlist_details`;
+
+CREATE TABLE `tbl_playlist_details` (
+  `id` varchar(50) NOT NULL,
+  `playlist_id` varchar(50) DEFAULT NULL,
+  `song_id` varchar(50) DEFAULT NULL,
+  `entry_by` varchar(50) DEFAULT NULL,
+  `entry_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Data for the table `tbl_playlist_details` */
+
+insert  into `tbl_playlist_details`(`id`,`playlist_id`,`song_id`,`entry_by`,`entry_at`) values ('17030593-8787-0320-455c-3ce8bce1410c','17030593-8786-2473-d46f-019662e546c2','17030498-8055-1368-37e8-c93422df4151','1','2023-12-20 13:33:07');
+
+/*Table structure for table `tbl_review` */
+
+DROP TABLE IF EXISTS `tbl_review`;
+
+CREATE TABLE `tbl_review` (
+  `id` varchar(50) DEFAULT NULL,
+  `song_id` varchar(50) DEFAULT NULL,
+  `overall_ratting` int(11) DEFAULT 0,
+  `bass_complexity` int(11) DEFAULT 0,
+  `drum_complexity` int(11) DEFAULT 0,
+  `bass_tone` int(11) DEFAULT 0,
+  `drum_sound` int(11) DEFAULT 0,
+  `is_slap` tinyint(4) DEFAULT 0,
+  `bass_solo` tinyint(4) DEFAULT 0,
+  `drum_solo` tinyint(4) DEFAULT 0,
+  `entry_by` varchar(50) DEFAULT NULL,
+  `entry_at` datetime DEFAULT NULL,
+  `update_by` varchar(50) DEFAULT NULL,
+  `update_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Data for the table `tbl_review` */
+
+insert  into `tbl_review`(`id`,`song_id`,`overall_ratting`,`bass_complexity`,`drum_complexity`,`bass_tone`,`drum_sound`,`is_slap`,`bass_solo`,`drum_solo`,`entry_by`,`entry_at`,`update_by`,`update_at`) values ('17030725-1151-2267-1efe-8824847a45ea','17030498-8055-1368-37e8-c93422df4151',4,4,4,4,4,1,0,0,'2','2023-12-20 17:11:51','2','2023-12-20 17:22:05'),('17030725-5828-2118-1c53-3ef7e437429c','17030498-8055-1368-37e8-c93422df4151',4,4,4,4,4,1,0,0,'1','2023-12-20 17:12:38',NULL,NULL);
+
 /*Table structure for table `tbl_roles` */
 
 DROP TABLE IF EXISTS `tbl_roles`;
@@ -134,7 +211,7 @@ CREATE TABLE `tbl_search_history` (
 
 /*Data for the table `tbl_search_history` */
 
-insert  into `tbl_search_history`(`id`,`search_type`,`search`,`entry_by`,`entry_at`) values ('17030532-7473-3021-2149-a28b5bf84bc7','artist','Artist','1','0000-00-00 00:00:00'),('17030532-9119-6299-8896-0b10b5074eab','artist','Artist','1','0000-00-00 00:00:00'),('17030533-0343-9949-f2eb-789240ad4576','artist','Artist next','1','0000-00-00 00:00:00'),('17030533-1861-1176-0971-509bd04e43a2','artist','Artist','1','0000-00-00 00:00:00'),('17030533-7878-1553-d356-682f6ab5405b','artist','Artist','1','2023-12-20 06:22:58'),('17030534-5209-9762-ac56-f31c1ab74080','artist','Artist','1','2023-12-20 11:54:12'),('17030570-0206-0272-0280-4836e82344b8','artist','Artist','1','2023-12-20 12:53:22'),('17030570-1545-8482-7e4e-f3ac54504f29','artist','Artist','1','2023-12-20 12:53:35'),('17030570-1832-2894-229d-3bf2438d41b5','artist','Artist','1','2023-12-20 12:53:38'),('17030570-7355-5107-300f-e3fae4644e04','artist','Artist','1','2023-12-20 12:54:33'),('17030570-7843-8455-5745-d09710e44edd','artist','Artist','1','2023-12-20 12:54:38');
+insert  into `tbl_search_history`(`id`,`search_type`,`search`,`entry_by`,`entry_at`) values ('17030532-7473-3021-2149-a28b5bf84bc7','artist','Artist','1','0000-00-00 00:00:00'),('17030532-9119-6299-8896-0b10b5074eab','artist','Artist','1','0000-00-00 00:00:00'),('17030533-0343-9949-f2eb-789240ad4576','artist','Artist next','1','0000-00-00 00:00:00'),('17030533-1861-1176-0971-509bd04e43a2','artist','Artist','1','0000-00-00 00:00:00'),('17030533-7878-1553-d356-682f6ab5405b','artist','Artist','1','2023-12-20 06:22:58'),('17030534-5209-9762-ac56-f31c1ab74080','artist','Artist','1','2023-12-20 11:54:12'),('17030570-0206-0272-0280-4836e82344b8','artist','Artist','1','2023-12-20 12:53:22'),('17030570-1545-8482-7e4e-f3ac54504f29','artist','Artist','1','2023-12-20 12:53:35'),('17030570-1832-2894-229d-3bf2438d41b5','artist','Artist','1','2023-12-20 12:53:38'),('17030570-7355-5107-300f-e3fae4644e04','artist','Artist','1','2023-12-20 12:54:33'),('17030570-7843-8455-5745-d09710e44edd','artist','Artist','1','2023-12-20 12:54:38'),('17030583-5297-2846-7c9a-5317dd7844ac','artist','Artist','1','2023-12-20 13:15:52'),('17030584-9762-8900-e955-98a53da6463e','artist','Artist','1','2023-12-20 13:18:17'),('17030585-5449-6303-9ae2-c761b66742b3','artist','Artist','1','2023-12-20 13:19:14'),('17030585-9393-2298-a2c9-577b40704eb2','artist','Artist','1','2023-12-20 13:19:53'),('17030734-2625-5959-6046-3f0fb99c416c','artist','Artist','1','2023-12-20 17:27:06'),('17030734-4509-6319-d17d-d5920bc74b48','artist','Artist','1','2023-12-20 17:27:25'),('17030734-7437-4130-ce93-dc93cd7241b8','artist','Artist','1','2023-12-20 17:27:54');
 
 /*Table structure for table `tbl_search_keyword` */
 
@@ -152,7 +229,7 @@ CREATE TABLE `tbl_search_keyword` (
 
 /*Data for the table `tbl_search_keyword` */
 
-insert  into `tbl_search_keyword`(`id`,`search_type`,`search`,`cnt`,`entry_by`,`entry_at`) values ('17030532-7473-9929-5013-a501937d4ba5','artist','Artist',10,'1','0000-00-00 00:00:00'),('17030533-0344-3540-9fa6-26d241c844bc','artist','Artist next',1,'1','0000-00-00 00:00:00');
+insert  into `tbl_search_keyword`(`id`,`search_type`,`search`,`cnt`,`entry_by`,`entry_at`) values ('17030532-7473-9929-5013-a501937d4ba5','artist','Artist',17,'1','0000-00-00 00:00:00'),('17030533-0344-3540-9fa6-26d241c844bc','artist','Artist next',1,'1','0000-00-00 00:00:00');
 
 /*Table structure for table `tbl_song_type` */
 
@@ -202,6 +279,7 @@ CREATE TABLE `tbl_songs` (
   `genre` varchar(100) DEFAULT NULL,
   `referance` longtext DEFAULT NULL,
   `from_csv` tinyint(4) DEFAULT 0,
+  `comment_cnt` int(11) DEFAULT NULL,
   `like_cnt` int(11) DEFAULT NULL,
   `avg_ratting` varchar(10) DEFAULT NULL,
   `entry_by` varchar(50) DEFAULT NULL,
@@ -213,7 +291,7 @@ CREATE TABLE `tbl_songs` (
 
 /*Data for the table `tbl_songs` */
 
-insert  into `tbl_songs`(`id`,`bass_player`,`title`,`artist_id`,`artist`,`album_id`,`album`,`apple_link`,`spotify_link`,`year`,`drummer`,`instruments`,`overall_ratting`,`bass_complexity`,`drum_complexity`,`bass_tone`,`drum_sound`,`is_slap`,`bass_solo`,`drum_solo`,`type_id`,`type`,`genre_id`,`genre`,`referance`,`from_csv`,`like_cnt`,`avg_ratting`,`entry_by`,`entry_at`,`update_by`,`update_at`) values ('17030498-8055-1368-37e8-c93422df4151','player','New Songs','17030493-6423-2973-983d-376fc38547f5','Artist','17030493-6421-5982-b579-338930a2415f','New Album','www.apple.com','www.spotify.com',2023,'Drummer','Drum',0,0,0,0,0,1,0,0,'17030493-6422-1542-f09c-6f56e32f4c3a','Type','17030493-6422-5138-adee-b1685cc74009','New Genre','None',0,0,NULL,'1','0000-00-00 00:00:00',NULL,NULL);
+insert  into `tbl_songs`(`id`,`bass_player`,`title`,`artist_id`,`artist`,`album_id`,`album`,`apple_link`,`spotify_link`,`year`,`drummer`,`instruments`,`overall_ratting`,`bass_complexity`,`drum_complexity`,`bass_tone`,`drum_sound`,`is_slap`,`bass_solo`,`drum_solo`,`type_id`,`type`,`genre_id`,`genre`,`referance`,`from_csv`,`comment_cnt`,`like_cnt`,`avg_ratting`,`entry_by`,`entry_at`,`update_by`,`update_at`) values ('17030498-8055-1368-37e8-c93422df4151','player','New Songs','17030493-6423-2973-983d-376fc38547f5','Artist','17030493-6421-5982-b579-338930a2415f','New Album','www.apple.com','www.spotify.com',2023,'Drummer','Drum',4,4,4,4,4,1,0,0,'17030493-6422-1542-f09c-6f56e32f4c3a','Type','17030493-6422-5138-adee-b1685cc74009','New Genre','None',0,1,0,NULL,'1','0000-00-00 00:00:00',NULL,NULL);
 
 /*Table structure for table `tbl_users` */
 
