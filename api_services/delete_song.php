82255e72249bf706394ab28a6c8ed7393cfcc2ab
Parth <mparth141@gmail.com>
2024-02-27 11:03:14 +0530

    manage delete songs

--- api_services/delete_song.php
+++ api_services/delete_song.php
@@ -8,6 +8,10 @@ function delete_song()
 	
 	if ($song_id && $login_user_id == 1) {
 		$db->delete("tbl_songs", array("id"=>$song_id));
+		$db->delete("tbl_comment_song", array("song_id"=>$song_id));
+		$db->delete("tbl_like_song", array("song_id"=>$song_id));
+		$db->delete("tbl_review", array("song_id"=>$song_id));
+		$db->delete("tbl_playlist_details", array("song_id"=>$song_id));
 		$outputjson['result'] = array();
 		$outputjson['success'] = 1;
 		$outputjson['message'] = "Song Deleted successfully";
